3f91ada6ee74a0a43076a2f4857a175e25b6abaa
Your <you@example.com>
2023-08-05 17:16:13 +0700

    Second-Commit

--- src/services/insert.js
+++ src/services/insert.js
@@ -29,6 +29,37 @@ const dataBody = [
     code: "NCT",
   },
 ];
+const categorys = [
+  {
+    code: "CTCH",
+    value: "Cho thuê căn hộ",
+    header: "Cho Thuê Căn Hộ Chung Cư, Giá Rẻ, Mới Nhất 2022",
+    subheader:
+      "Cho thuê căn hộ - Kênh đăng tin cho thuê căn hộ số 1: giá rẻ, chính chủ, đầy đủ tiện nghi. Cho thuê chung cư với nhiều mức giá, diện tích cho thuê khác nhau.",
+  },
+  {
+    code: "CTMB",
+    value: "Cho thuê mặt bằng",
+    header:
+      "Cho Thuê Mặt Bằng, Cho Thuê Văn Phòng, Cửa Hàng, Kiot, Mới Nhất 2022",
+    subheader:
+      "Cho thuê mặt bằng - Kênh đăng tin cho thuê mặt bằng, cho thuê cửa hàng, cho thuê kiot số 1: giá rẻ, mặt tiền, khu đông dân cư, phù hợp kinh doanh.",
+  },
+  {
+    code: "CTPT",
+    value: "Cho thuê phòng trọ",
+    header: "Cho Thuê Phòng Trọ, Giá Rẻ, Tiện Nghi, Mới Nhất 2022",
+    subheader:
+      "Cho thuê phòng trọ - Kênh thông tin số 1 về phòng trọ giá rẻ, phòng trọ sinh viên, phòng trọ cao cấp mới nhất năm 2022. Tất cả nhà trọ cho thuê giá tốt nhất tại Việt Nam.",
+  },
+  {
+    code: "NCT",
+    value: "Nhà cho thuê",
+    header: "Cho Thuê Nhà Nguyên Căn, Giá Rẻ, Chính Chủ, Mới Nhất 2022",
+    subheader:
+      "Cho thuê nhà nguyên căn - Kênh đăng tin cho thuê nhà số 1: giá rẻ, chính chủ, miễn trung gian, đầy đủ tiện nghi, mức giá, diện tích cho thuê khác nhau.",
+  },
+];
 const hashPassword = (password) =>
   bcrypt.hashSync(password, bcrypt.genSaltSync(12));
 
@@ -37,6 +68,21 @@ export const insertService = () =>
     try {
       const labelCodes = [];
       const provinceCodes = [];
+      await db.Category.bulkCreate(categorys);
+      dataPrice.forEach(async (item, index) => {
+        await db.Price.create({
+          code: item.code,
+          value: item.value,
+          order: index + 1,
+        });
+      });
+      dataArea.forEach(async (item, index) => {
+        await db.Area.create({
+          code: item.code,
+          value: item.value,
+          order: index + 1,
+        });
+      });
       dataBody.forEach((care) => {
         care.body.forEach(async (item) => {
           let postId = v4();
